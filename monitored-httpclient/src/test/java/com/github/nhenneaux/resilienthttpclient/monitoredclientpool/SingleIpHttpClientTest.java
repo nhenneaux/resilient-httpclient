1ca1594b8552f399846ac2048645311d01762acb
hemant.ghodke <hemant.ghodke@worldline.com>
2024-04-02 14:51:59 +0530

    AHUB-19894 added POST test case

--- monitored-httpclient/src/test/java/com/github/nhenneaux/resilienthttpclient/monitoredclientpool/SingleIpHttpClientTest.java
+++ monitored-httpclient/src/test/java/com/github/nhenneaux/resilienthttpclient/monitoredclientpool/SingleIpHttpClientTest.java
@@ -4,6 +4,9 @@ import com.github.nhenneaux.resilienthttpclient.singlehostclient.DnsLookupWrappe
 import com.github.nhenneaux.resilienthttpclient.singlehostclient.ServerConfiguration;
 import com.github.nhenneaux.resilienthttpclient.singlehostclient.SingleHostHttpClientBuilder;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 
 import java.net.InetAddress;
@@ -13,20 +16,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import static com.github.nhenneaux.resilienthttpclient.monitoredclientpool.HttpClientPoolTest.PUBLIC_HOST_TO_TEST;
+import static com.github.nhenneaux.resilienthttpclient.singlehostclient.ServerConfiguration.DEFAULT_REQUEST_TRANSFORMER;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class SingleIpHttpClientTest {
     @SuppressWarnings("unchecked")
@@ -75,13 +73,12 @@ class SingleIpHttpClientTest {
         // Given
         final String hostname = PUBLIC_HOST_TO_TEST.get(0);
         final HttpClient httpClient = mock(HttpClient.class);
-        final Consumer<HttpRequest.Builder> requestTransformer = getRequestTransformer();
         ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
         @SuppressWarnings("unchecked") final HttpResponse<Void> httpResponse = mock(HttpResponse.class);
         when(httpResponse.statusCode()).thenReturn(500);
         when(httpClient.sendAsync(captor.capture(), any(DISCARDING_BODY_HANDLER_CLASS))).thenReturn(CompletableFuture.completedFuture(httpResponse));
 
-        ServerConfiguration serverConfiguration = new ServerConfiguration(hostname, 443, "/", 1, 1, -1, 1, requestTransformer);
+        ServerConfiguration serverConfiguration = new ServerConfiguration(hostname, 443, "/", 1, 1, -1, 1, DEFAULT_REQUEST_TRANSFORMER);
 
         // When
         try (final SingleIpHttpClient singleIpHttpClient = new SingleIpHttpClient(httpClient, new DnsLookupWrapper().getInetAddressesByDnsLookUp(hostname).iterator().next(), serverConfiguration)) {
@@ -130,9 +127,8 @@ class SingleIpHttpClientTest {
     void shouldFailOnMalformedUrl() throws UnknownHostException {
         // Given
         final HttpClient httpClient = HttpClient.newHttpClient();
-        final Consumer<HttpRequest.Builder> requestTransformer = getRequestTransformer();
         // When - Then
-        ServerConfiguration serverConfiguration = new ServerConfiguration("com.github.nhenneaux.resilienthttpclient.monitoredclientpool.SingleIpHttpClientTest.shouldCreateClientWithoutRefresh", -234, "&dfsfsd", 1, 1, -1, 0, requestTransformer);
+        ServerConfiguration serverConfiguration = new ServerConfiguration("com.github.nhenneaux.resilienthttpclient.monitoredclientpool.SingleIpHttpClientTest.shouldCreateClientWithoutRefresh", -234, "&dfsfsd", 1, 1, -1, 0, DEFAULT_REQUEST_TRANSFORMER);
         InetAddress localHost = InetAddress.getLocalHost();
         final IllegalArgumentException illegalStateException = assertThrows(IllegalArgumentException.class, () -> new SingleIpHttpClient(httpClient, localHost, serverConfiguration));
         assertEquals("Cannot build health URI from ServerConfiguration{hostname='com.github.nhenneaux.resilienthttpclient.monitoredclientpool.SingleIpHttpClientTest.shouldCreateClientWithoutRefresh', port=-234, healthPath='&dfsfsd', connectionHealthCheckPeriodInSeconds=1, dnsLookupRefreshPeriodInSeconds=1, healthReadTimeoutInMilliseconds=-1, failureResponseCountThreshold= 0}", illegalStateException.getMessage());
@@ -173,7 +169,33 @@ class SingleIpHttpClientTest {
         }
     }
 
-    static Consumer<HttpRequest.Builder> getRequestTransformer() {
-        return builder -> builder.POST(HttpRequest.BodyPublishers.ofString("{\"request\":{\"transactionType\":\"ECHO_TEST\"}}"));
+    @ParameterizedTest
+    @MethodSource("getArgumentsForHealthCheckPostRequest")
+    void shouldBeHealthyWithPostRequest(final String givenHealthCheckRequestBody, final boolean expectedIsHealthy) {
+        // Given
+        final String hostname = PUBLIC_HOST_TO_TEST.get(0);
+        final InetAddress ip = new DnsLookupWrapper().getInetAddressesByDnsLookUp(hostname).iterator().next();
+        final HttpClient httpClient = SingleHostHttpClientBuilder.newHttpClient(hostname, ip);
+        // When
+        final Consumer<HttpRequest.Builder> requestTransformer = getRequestTransformer(givenHealthCheckRequestBody);
+        try (final SingleIpHttpClient singleIpHttpClient = new SingleIpHttpClient(httpClient, ip, new ServerConfiguration(hostname, 443, "/", 1, 1, -1, 1, requestTransformer))) {
+
+            // Then
+            assertSame(httpClient, singleIpHttpClient.getHttpClient());
+            assertEquals(expectedIsHealthy, singleIpHttpClient.isHealthy());
+            assertThat("failedResponseCount", singleIpHttpClient.getFailedResponseCount(), equalTo(0));
+        }
+    }
+
+    private static Stream<Arguments> getArgumentsForHealthCheckPostRequest() {
+        return Stream.of(
+                Arguments.of("{\"request\":{\"transactionType\":\"ECHO_TEST\"}}", true),
+                Arguments.of("", false),
+                Arguments.of(null, false)
+        );
+    }
+
+    static Consumer<HttpRequest.Builder> getRequestTransformer(String requestBody) {
+        return builder -> builder.POST(HttpRequest.BodyPublishers.ofString(requestBody));
     }
 }
