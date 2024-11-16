4b4b4d3e69458bc8271352476339b394ad8467de
Nicolas Henneaux <nicolas@henneaux.io>
2024-11-16 13:59:00 +0100

    Fix integration test with nicolas.henneaux.io failing some tests, using another domain

--- monitored-httpclient/src/test/java/com/github/nhenneaux/resilienthttpclient/monitoredclientpool/HttpClientPoolTest.java
+++ monitored-httpclient/src/test/java/com/github/nhenneaux/resilienthttpclient/monitoredclientpool/HttpClientPoolTest.java
@@ -656,7 +656,7 @@ class HttpClientPoolTest {
     @Timeout(20)
     void shouldConnectTimeoutEmptyElement() {
         // Given
-        final String hostname = PUBLIC_HOST_TO_TEST.get(1);
+        final String hostname = PUBLIC_HOST_TO_TEST.get(3);
         final ServerConfiguration serverConfiguration = new ServerConfiguration(hostname);
         mockScheduledExecutorService(serverConfiguration);
 
